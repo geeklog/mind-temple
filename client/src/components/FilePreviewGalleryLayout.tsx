f5f8b22ff6369b4924669c4b56e8179f762eed46
Mike Wu <mike@bitguild.com>
2020-10-30 23:46:04 +0800

    style: reformat

--- client/src/components/FilePreviewGalleryLayout.tsx
+++ client/src/components/FilePreviewGalleryLayout.tsx
@@ -23,10 +23,6 @@ class Gallery extends React.PureComponent<{
                 key={i}
                 index={i}
                 style={{ paddingBottom: 'calc(90vh - 25px)' }}
-                onFocus={() => {
-                  console.log('onFocus', i)
-                  control.setCurrIndex(i)
-                }}
               >
                 <div className="image-box">
                   <ButtonBack className="btn btn-back">
@@ -34,14 +30,20 @@ class Gallery extends React.PureComponent<{
                   </ButtonBack>
                   <div className="image-frame">
                     <Image
-                        key={i}
-                        className="image"
-                        hasMasterSpinner={false}
-                        src={`${apiServer}/file/${file.path}`}
-                        alt={file.name}
+                      key={i}
+                      className="image"
+                      hasMasterSpinner={false}
+                      src={`${apiServer}/file/${file.path}`}
+                      alt={file.name}
                     />
-                    <div className="image-frame-overlay-left" onClick={control.selectPrev} />
-                    <div className="image-frame-overlay-right" onClick={control.selectNext} />
+                    <div
+                      className="image-frame-overlay-left"
+                      onClick={control.selectPrev}
+                    />
+                    <div
+                      className="image-frame-overlay-right"
+                      onClick={control.selectNext}
+                    />
                   </div>
                   <ButtonNext className="btn btn-next">
                     <Icon name="chevron-right"/>
@@ -67,7 +69,7 @@ export default class FilePreviewGalleryLayout extends React.PureComponent<{
   control: AppControl;
 }> {
   render() {
-    const {files, currIndex, control} = this.props;
+    const {files, control} = this.props;
     return (
       <CarouselProvider
          naturalSlideWidth={100}
