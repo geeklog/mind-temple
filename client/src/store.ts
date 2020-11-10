cb36808c545cf4a781ca4a3049e23cbce8083708
Mike Wu <mike@bitguild.com>
2020-11-10 15:19:56 +0800

    feat: persist redux

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,9 +1,20 @@
 import { init, RematchDispatch, RematchRootState } from '@rematch/core';
+import createRematchPersist from '@rematch/persist';
+import storage from 'redux-persist/lib/storage'
 import { models, RootModel } from './models';
 
-export const store = init({
-	models,
+const persistPlugin: any = createRematchPersist({
+  key: 'root',
+  storage,
+  whitelist: ['app'],
+  throttle: 1000,
+  version: 1,
 })
+
+export const store = init({
+  models,
+  plugins: [persistPlugin]
+});
 
 export type Store = typeof store;
 export type Dispatch = RematchDispatch<RootModel>;
