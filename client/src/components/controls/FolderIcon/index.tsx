76059841d280615b1e31cfead3d86b9516166ab8
Mike Wu <mike@bitguild.com>
2020-11-19 00:45:55 +0800

    feat: colorful folders

--- client/src/components/controls/FolderIcon/index.tsx
+++ client/src/components/controls/FolderIcon/index.tsx
@@ -4,18 +4,31 @@ import classnames from 'classnames';
 
 export default class FolderIcon extends React.Component<{
   className?: string,
-  size: 'large' | 'medium' | 'small'
+  size: 'large' | 'medium' | 'small',
+  color?: string
 }> {
   render() {
-    const {size} = this.props;
+    const {size, color} = this.props;
+    const colorClass = color ? color : 'blue';
     return (
       <div className={classnames(
         "folder-icon",
+        colorClass,
         this.props.className,
         size
       )}>
-        <div className={classnames("top-part")} />
-        <div className={classnames("body-part")} />
+        <div
+          className={classnames(
+            "top-part",
+            colorClass,
+          )}
+        />
+        <div
+          className={classnames(
+            "body-part",
+            colorClass,
+          )}
+        />
         {this.props.children}
       </div>
       );
